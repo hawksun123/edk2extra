ea8724bf5fc459fa8b2c7ffa441e61166492a508
Hawk_Sun <hawksun123@gmail.com>
2020-03-16 09:31:15 +0800

    delete archive totally !!

--- archive/edk2/b.bat
+++
@@ -1,5 +0,0 @@
-if "%WORKSPACE%" == "" call env.bat
-build -p EmulatorPkg\EmulatorPkg.dsc -t VS2017 -a X64 %*
-:quit
-
-
--- archive/edk2/r.bat
+++
@@ -1,3 +0,0 @@
-pushd
-cd Build\EmulatorX64\DEBUG_VS2017\X64\ && WinHost.exe
-popd
